a77fa5cad2ee19cf714980159e92fb6ed18084e2
Mikhail <mikhail@delport.ca>
2019-07-29 14:27:25 -0400

    Update title and description

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,14 +10,14 @@ export default {
    ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
    */
   head: {
-    title: "Nuxt.js starter for CSB",
+    title: "The Font Page",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
         hid: "description",
         name: "description",
-        content: "Official Nuxt.js starter for CodeSandBox"
+        content: "HackerNews Front PAge"
       }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
