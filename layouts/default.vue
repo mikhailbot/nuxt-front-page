3490b1e8afa4063e014e6c6cf12ec9f33bde1b41
Mikhail <mikhail@delport.ca>
2019-07-29 14:23:29 -0400

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="max-w-xl mx-auto my-4 p-2 bg-gray-100 rounded">
+    <h1 class="text-3xl font-bold mb-2 pl-2">The Front Page</h1>
+    <Nuxt/>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="flex flex-col">
+    <a
+      v-for="link in links"
+      :key="link.id"
+      :href="link.url"
+      target="_blank"
+      class="m-2 py-2 px-4 font-sans bg-white shadow hover:shadow-lg rounded inline-block"
+    >
+      <span class="block text-gray-700 text-sm font-semibold">{{ link.time_ago }}</span>
+      <span class="text-lg font-semibold">{{ link.title }}</span>
+    </a>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      now: undefined
+    };
+  },
+
+  components: {},
+  async asyncData({ $axios }) {
+    let { data } = await $axios.get("/news");
+    return { links: data };
+  }
+};
+</script>
