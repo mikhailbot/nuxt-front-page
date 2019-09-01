6b4bfe4451fbfa0eaa608c49271facad5a599231
Mikhail Delport <mikhail@delport.ca>
2019-08-31 20:14:33 -0400

    Move items to component, handle ask hn links

---
+++ components/NewsLink.vue
@@ -0,0 +1,26 @@
+<template>
+  <a
+    :href="url"
+    target="_blank"
+    class="m-2 py-2 px-4 font-sans bg-white shadow hover:shadow-lg rounded block"
+  >
+    <span class="block text-gray-700 text-sm">{{ link.time_ago }}</span>
+    <span class="text-lg font-semibold">{{ link.title }}</span>
+  </a>
+</template>
+
+<script>
+export default {
+  props: ["link"],
+
+  computed: {
+    url: function() {
+      if (this.link.type === "ask") {
+        return `https://news.ycombinator.com/${this.link.url}`;
+      } else {
+        return this.link.url;
+      }
+    }
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,19 @@
 <template>
   <section class="flex flex-col">
-    <a
-      v-for="link in links"
-      :key="link.id"
-      :href="link.url"
-      target="_blank"
-      class="m-2 py-2 px-4 font-sans bg-white shadow hover:shadow-lg rounded inline-block"
-    >
-      <span class="block text-gray-700 text-sm font-semibold">{{ link.time_ago }}</span>
-      <span class="text-lg font-semibold">{{ link.title }}</span>
-    </a>
+    <div v-for="link in links" v-bind:key="link.id">
+      <news-link v-bind:link="link" />
+    </div>
   </section>
 </template>
 
 <script>
+import NewsLink from "~/components/NewsLink.vue";
+
 export default {
-  data() {
-    return {
-      now: undefined
-    };
+  components: {
+    NewsLink
   },
 
-  components: {},
   async asyncData({ $axios }) {
     let { data } = await $axios.get("/news");
     return { links: data };
